6b84bd7aea15e1c8b70ff27932aea833fc84bae2
asoulkani <asoulkani@gmail.com>
2022-10-02 14:48:25 +0100

    add error property

--- src/app/error-list-element/error-list-element.component.ts
+++ src/app/error-list-element/error-list-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Error } from '../Models/Error.model';
 
 @Component({
   selector: '[app-error-list-element]',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error-list-element.component.css']
 })
 export class ErrorListElementComponent implements OnInit {
+
+  spanLevelClass: string = "bg-danger";
+  spanStatusClass: string = "bg-danger";
+  @Input() error: Error = new Error(0,"","","Urgent","Open");
 
   constructor() { }
 
