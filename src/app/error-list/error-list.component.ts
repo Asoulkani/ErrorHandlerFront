c8fe279f3b436eacb531bedda1c7c61d09b1866e
a.soulkani <a.soulkani@gmail.com>
2022-10-07 00:58:03 +0200

    adding deleteALL, deleteSelected, restoreLastDeleted Buttons

--- src/app/error-list/error-list.component.ts
+++ src/app/error-list/error-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,EventEmitter,Output } from '@angular/core';
 import { Application } from '../Models/application.model';
 import { Error } from '../Models/Error.model';
 
@@ -9,11 +9,20 @@ import { Error } from '../Models/Error.model';
 })
 export class ErrorListComponent implements OnInit {
 
+  @Output() elementChecked: EventEmitter<[Application,Error]> = new EventEmitter<[Application,Error]>();
+  @Output() elementUnChecked: EventEmitter<[Application,Error]> = new EventEmitter<[Application,Error]>();
   @Input() app: Application = new Application(0,"");
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addElementChecked(error: Error){
+    this.elementChecked.emit([this.app,error]);
+  }
+  removeElementChecked(error: Error){
+    this.elementUnChecked.emit([this.app,error]);
   }
 
 }
